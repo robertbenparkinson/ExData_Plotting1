f826af53918161dfb9bd8d9b0ce89244affbbba1
robertbenparkinson <robertbenparkinson@gmail.com>
2016-02-13 10:23:05 +0000

    updates

--- plot1.R
+++ plot1.R
@@ -1,15 +1,30 @@
-##Get the full data set
-##Had problems trying to use "dlypr" library
+##PLOT 1
 
-hpc <- read.table("household_power_consumption.txt", sep =";", dec = ".", header = TRUE, na.strings ="?",
-                  stringsAsFactors = FALSE, check.names=FALSE, comment.char="", quote='\"')
+##load key libraries
+library(dplyr)
+library(R.utils)
+
+##file url
+u <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+
+##check and download household power consumptin  file 
+if (!file.exists("household_power_consumption.zip")) {
+  download.file(u, destfile = "household_power_consumption.zip") 
+}
+
+##unzip file
+unz("household_power_consumption.zip") 
+
+##read file
+hpc <- read.table("household_power_consumption.txt", sep =";", dec = ".", 
+                  header = TRUE, na.strings ="?", stringsAsFactors = FALSE, 
+                  check.names=FALSE, comment.char="", quote='\"')
 
 ##Converted to date and saved back to itself
 hpc$Date <- as.Date(hpc$Date, format="%d/%m/%Y")
 
 ## Subsetting the data
 hpc_sub <- subset(hpc, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
-
 
 ## Converting dates
 datetime <- paste(as.Date(hpc_sub$Date), hpc_sub$Time)
--- plot4.R
+++ plot4.R
@@ -1,9 +1,24 @@
-##Get the full data set
-##Had problems trying to use "dlypr" library
+##PLOT 4
 
-hpc <- read.table("household_power_consumption.txt", sep =";", dec = ".", header = TRUE, na.strings ="?",
-                  stringsAsFactors = FALSE, check.names=FALSE, comment.char="", quote='\"')
+##load key libraries
+library(dplyr)
+library(R.utils)
 
+##file url
+u <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+
+##check and download household power consumptin  file 
+if (!file.exists("household_power_consumption.zip")) {
+  download.file(u, destfile = "household_power_consumption.zip") 
+}
+
+##unzip file
+unz("household_power_consumption.zip") 
+
+##read file
+hpc <- read.table("household_power_consumption.txt", sep =";", dec = ".", 
+                  header = TRUE, na.strings ="?", stringsAsFactors = FALSE, 
+                  check.names=FALSE, comment.char="", quote='\"')
 ##Converted to date and saved back to itself
 hpc$Date <- as.Date(hpc$Date, format="%d/%m/%Y")
 
